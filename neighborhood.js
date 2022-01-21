18ebff15d2825a4f9aa31b8c2f58209573941c02
Benberto <ben.bertagnole@gmail.com>
2022-01-21 13:22:31 -0700

    corrected spelling error

--- neighborhood.js
+++ neighborhood.js
@@ -1,15 +1,14 @@
 let restArr = ['The Pie Pizzeria', 'Slackwater', "Tony's"]
 
 const random = Math.floor(Math.random() * restArr.length);
+
 function randomRest(evt) {
 	evt.preventDefault();
 	
-	let name = random + restArr[random]
-
-  alert(name)
+	alert('You should eat at ' +restArr[random]+'!')
 }
 
 
 let rest = document.querySelector('button#rest')
 
-color.addEventListener('click', randomRest);
+rest.addEventListener('click', randomRest);
